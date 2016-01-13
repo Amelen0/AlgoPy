2fa61e48e0865c90e03cea08e6b69b1e04375d47
AbdielMelendez/ <abdielhmelendez@gmail.com>
2016-01-12 16:30:04 -0800

    New Script

---
+++ Factorialize.py
@@ -0,0 +1,10 @@
+def Factorialize(num):
+   if num == 0:
+      return 1
+
+   return num * (num - 1)
+
+
+
+
+Factorialize(5)
---
+++ Reverse.py
@@ -0,0 +1,8 @@
+"""Reverse a string in Python"""
+
+def Reverse(str):
+   return str[::-1]
+
+
+
+print Reverse("Hello")
